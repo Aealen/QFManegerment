2795d1b207930cf62e9df0ad4583d988f0cb55e0
DareSasuke <yzbzzzy@gmail.com>
2022-12-03 11:38:19 +0800

    订单查看

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -3,23 +3,51 @@
         <el-table
       :data="tableData"
       style="width: 100%">
+         <el-table-column type="expand" >
+      <template slot-scope="props">
+        <el-form  inline class="demo-table-expand" >
+           <el-table
+      :data="props.row.orders"
+      style="width: 100%">
       <el-table-column
-        prop="Uid"
+        prop="oid"
         label="id"
         width="180">
       </el-table-column>
       <el-table-column
-        prop="UserName"
+        prop="status"
+        label="状态"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="cdk"
+        label="cdk">
+      </el-table-column>
+            <el-table-column
+        prop="wid"
+        label="商品">
+      </el-table-column>
+    </el-table>
+        </el-form>
+      </template>
+    </el-table-column>
+      <el-table-column
+        prop="user.Uid"
+        label="id"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="user.UserName"
         label="名称"
         width="180">
       </el-table-column>
       <el-table-column
-        prop="EMail"
+        prop="user.EMail"
         label="邮箱"
         width="180">
       </el-table-column>
         <el-table-column
-        prop="balance"
+        prop="user.balance"
         label="余额"
         width="480"
         >
@@ -47,15 +75,37 @@
     </div>
 </template>
 <script>
+import axios from 'axios'
 export default {
     data() {
         return {
-          tableData: []
+          tableData: [],
+          orderData:[],
+          getRowKeys(row) {
+          return row.Uid;
+          },
+           expands: [],
         }
       },
       methods:{
+
+        // get(row){
+        //   console.log(row.user.Uid)
+        //   axios({
+        //     method:'get',
+        //     url:'/ff14/Order/GetAllOrderByUid?Uid='+row.user.Uid,
+        //   }).then(res=>{
+        //     console.log(res.data)
+        //     this.$data.orderData=[]
+        //     if(res.data.code=='1'){
+        //       this.$data.orderData=[]
+        //     }else{
+        //       this.$data.orderData=res.data.data
+        //     }
+        //   })
+        // },
         change(index,row){
-        console.log(this.$data.tableData[index].Uid)  
+        console.log(this.$data.tableData[index].user.Uid)  
         this.$prompt('请输入余额', '提示', {
           confirmButtonText: '确定',
           cancelButtonText: '取消',
@@ -71,11 +121,11 @@ export default {
           });
                       this.$axios({
               method:'put',
-              url: '/ff14/login/upBalance?Uid='+this.$data.tableData[index].Uid+'&Balance='+parseFloat(value),
+              url: '/ff14/login/upBalance?Uid='+this.$data.tableData[index].user.Uid+'&Balance='+parseFloat(value),
             }).then(res=>{
               this.$axios({
                     method:'get',
-                    url: "/ff14/login/getAllUsers",
+                    url: "/ff14/login/getOrders",
                 }).then(res=>{
                     console.log(res.data);
                     this.tableData=res.data.data
@@ -108,7 +158,7 @@ export default {
                 }).then(res=>{
                 this.$axios({
                     method:'get',
-                    url: "/ff14/login/getAllUsers",
+                    url: "/ff14/login/getOrders",
                 }).then(res=>{
                     console.log(res.data);
                     this.tableData=res.data.data
@@ -125,7 +175,7 @@ export default {
     beforeCreate(){
         this.$axios({
             method:'get',
-            url: "/ff14/login/getAllUsers",
+            url: "/ff14/login/getOrders",
         }).then(res=>{
             console.log(res.data);
             this.tableData=res.data.data
